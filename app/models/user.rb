430dace72b4e5b46cf0cac11149f3f3fc180f0f8
Marco Gallardo <marco.gallardo@tangosource.com>
2012-02-04 10:40:08 -0600

    search working

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ActiveRecord::Base
+  scope :enabled_users, where(:enabled => true)
+
+  def self.search(value)
+    where('first_name LIKE ? OR last_name LIKE ? OR email LIKE ? OR address LIKE ?', value, value, value, value)
+  end
+
 end
